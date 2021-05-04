9e86d83354d5f00af072c06f50f25dd0a725c54e
Mikdaam <mikdaam.badarou@etu.upec.fr>
2021-05-04 15:41:33 +0200

    My Kanban

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { Task } from './task/task';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Kanban';
+  todo: Task[] = [
+    { title: 'Buy milk', description: 'Go to the store and buy milk' },
+    { title: 'Create Kanban board', description: 'Develop a Kanban bord' }
+  ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
+
+  edit(list: string, task: Task): void {
+
+  }
+
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task: {}
+      }
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult) => this.todo.push(result.task));
+  }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from './task';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.scss']
+})
+export class TaskComponent implements OnInit {
+
+  @Input() task: Task;
+  @Output() edit = new EventEmitter();
+
+  constructor() { 
+    this.task = {title: '', description: ''};
+   }
+
+  ngOnInit(): void {
+  }
+
+}
