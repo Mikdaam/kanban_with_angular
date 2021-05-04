baa5ab42e8dff88685415f680690c3978c372256
Mikdaam <mikdaam.badarou@etu.upec.fr>
2021-05-04 20:06:41 +0200

    Deploy on github

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBczqqKTS-K40khf0u_UQmaD2Mq3x2DeXM",
-    authDomain: "kanban-backend-89fb7.firebaseapp.com",
-    projectId: "kanban-backend-89fb7",
-    storageBucket: "kanban-backend-89fb7.appspot.com",
-    messagingSenderId: "30733385398",
-    appId: "1:30733385398:web:586956339d51a2e178b017",
-    measurementId: "G-53NW9FDLWJ"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID"
   }
 };
 
